3f7ccc4c7eaaaf93a1f60a10e23e5185c45ebfe5
DrDisagree <29881338+Mahmud0808@users.noreply.github.com>
2026-08-26 18:09:15 +0600

    Bump version code to 4 and version name to 1.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.drdisagree.teledrive"
         minSdk = 26
         targetSdk = 37
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 4
+        versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
